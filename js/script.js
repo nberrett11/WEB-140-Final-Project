820bbe80d26fa6a9e787a0976b49fd89d5fe876e
Jonathan <j.rasberry@outlook.com>
2018-04-27 21:14:41 -0400

    Course Dropdowns added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
-    //Welcome alert
+
+    //Welcome alert when website first loads
     alert("We want to thank you for visiting our site.");
+
+    // Drops down all course requirment tables
+    $('.dd-btn').click(function() {
+        $('.dropDwnAll').slideToggle();
+    });
+
+    //Drop down when table header is clicked
+
+    $('.dd1').click(function() {
+        $('.dropDwn1').slideToggle();
+    });
+    $('.dd2').click(function() {
+        $('.dropDwn2').slideToggle();
+    });
+    $('.dd3').click(function() {
+        $('.dropDwn3').slideToggle();
+    });
+    $('.dd4').click(function() {
+        $('.dropDwn4').slideToggle();
+    });
+    $('.dd5').click(function() {
+        $('.dropDwn5').slideToggle();
+    });
+    $('.dd6').click(function() {
+        $('.dropDwn6').slideToggle();
+    });
+    $('.dd7').click(function() {
+        $('.dropDwn7').slideToggle();
+    });
+
+
 });
